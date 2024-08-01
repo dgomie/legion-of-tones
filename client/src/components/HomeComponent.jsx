5caa16d351c4dcb7765bc5f0dd7f218ff7a49540
Danny <danny.f.gomez@gmail.com>
2024-08-01 14:17:16 -0400

    add description to homepage

--- client/src/components/HomeComponent.jsx
+++ client/src/components/HomeComponent.jsx
@@ -1,32 +1,27 @@
 import { useNavigate, useLocation } from 'react-router-dom';
 import {
-    Container,
-    Box,
-    Typography,
-    Button,
-    Paper,
-    Card,
-    CardContent,
-  } from "@mui/material";
-  import Auth from "../utils/auth"
-  import hero from "../images/lot-hero.png";
-  
+  Container,
+  Box,
+  Typography,
+  Button,
+  Paper,
+  Card,
+  CardContent,
+} from '@mui/material';
+import Auth from '../utils/auth';
+import DescriptionComponent from './DescriptionComponent';
+import hero from '../images/lot-hero.png';
+
+const HomeComponent = () => {
 
 
-  const HomeComponent = () => {
-    const isLoggedIn = Auth.loggedIn();
-    const navigate = useNavigate(); 
-
-    return (
-      <div style={{paddingBottom:'50px'}}>
+  return (
+    <div style={{ paddingBottom: '50px' }}>
       <img src={hero} width="100%" alt="" />
-      {!isLoggedIn && (
-      <Box sx={{ padding: 1, display: "flex", justifyContent:"center"}}>
-      <Button sx={{margin:1}} variant="contained" onClick={() => {navigate('/login')}}>Login</Button>
-      <Button sx={{margin:1}} variant="contained" onClick={() => {navigate('/signup')}}>Sign Up</Button>
-      </Box>
-      )}
-      </div>
-    );
-  };
-  export default HomeComponent;
+ 
+
+     <DescriptionComponent />
+    </div>
+  );
+};
+export default HomeComponent;
