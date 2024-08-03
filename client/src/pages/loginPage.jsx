807fa2a28d84e07be01ddb8affbf8c38ba0055c5
Danny <danny.f.gomez@gmail.com>
2024-08-03 14:38:13 -0400

    change redirect on login page to dashboard

--- client/src/pages/loginPage.jsx
+++ client/src/pages/loginPage.jsx
@@ -10,7 +10,7 @@ function LoginPage() {
         const isLoggedIn = Auth.loggedIn(); 
 
         if (isLoggedIn) {
-            navigate('/profile'); 
+            navigate('/dashboard'); 
         }
     }, [navigate]);
 
