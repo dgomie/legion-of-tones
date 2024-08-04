2f9c544b6b9da17de6065fa706fe4b636ce1ed88
Danny <danny.f.gomez@gmail.com>
2024-08-03 20:21:33 -0400

    add validation for create legions page

--- client/src/pages/CreateLegionPage.jsx
+++ client/src/pages/CreateLegionPage.jsx
@@ -1,11 +1,28 @@
-import CreateLegionComponent from "../components/CreateLegionComponent";
+import CreateLegionComponent from '../components/CreateLegionComponent';
+import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import Auth from '../utils/auth';
 
 function CreateLegionPage() {
+  const navigate = useNavigate();
+  const [isLoggedIn, setIsLoggedIn] = useState(Auth.loggedIn());
+
+  useEffect(() => {
+    setIsLoggedIn(Auth.loggedIn());
+
+    if (!isLoggedIn) {
+      navigate('/');
+    }
+  }, [navigate, isLoggedIn]);
+
+  if (!isLoggedIn) {
+    return null;
+  }
   return (
-  <>
-  <CreateLegionComponent />
-  </>)
-  ;
+    <>
+      <CreateLegionComponent />
+    </>
+  );
 }
 
 export default CreateLegionPage;
