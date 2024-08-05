6c1f2c706bd2a59d7792fdbca1158d2914180244
Danny <danny.f.gomez@gmail.com>
2024-08-05 01:53:51 -0400

    add user profilepicture and link to user profile page

--- client/src/components/PlayerName.jsx
+++ client/src/components/PlayerName.jsx
@@ -1,19 +1,35 @@
 import React from "react";
 import { useQuery } from '@apollo/client';
 import { GET_USER_BY_ID } from "../utils/queries";
-import { Typography } from "@mui/material";
+import { Avatar, Typography } from "@mui/material";
+import { Link } from 'react-router-dom';
 
 const PlayerName = ({ playerId }) => {
   const { loading, error, data } = useQuery(GET_USER_BY_ID, {
     variables: { id: playerId },
   });
 
-  console.log(data)
+  console.log(data);
 
   if (loading) return <Typography>Loading...</Typography>;
   if (error) return <Typography>Error: {error.message}</Typography>;
 
-  return <Typography>{data.userById.username}</Typography>;
+  return (
+    <Typography>
+      <Link to={`/profile/${data.userById.username}`}>
+        <Avatar
+          alt="Profile Picture"
+          className="avatar"
+          sx={{
+            width: '75px',
+            height: '75px',
+            marginBottom: 2,
+          }}
+          src={data.userById.profilePicture}
+        />
+      </Link>
+    </Typography>
+  );
 };
 
 export default PlayerName;
