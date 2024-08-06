560a649f4ffef9edb1cacbd781d6819b5123cf70
Danny <danny.f.gomez@gmail.com>
2024-08-06 10:19:27 -0400

    updated copy on about component

--- client/src/components/AboutComponent.jsx
+++ client/src/components/AboutComponent.jsx
@@ -1,18 +1,15 @@
-import { Box, Typography, Divider } from "@mui/material";
+import { Box, Typography, Divider, Button } from '@mui/material';
 
-  const AboutComponent = () => {
-    
-
-    return (
+const AboutComponent = () => {
+  return (
+    <Box>
+      <Typography variant='h3' sx={{fontFamily: "medievalSharp"}}>   About Us</Typography>
+      <Divider sx={{ marginY: 2 }} />
       <Box>
-        About Page Under Construction
-        <Divider sx={{marginY: 2}}/>
-        <Box>
-          <Typography sx={{fontWeight: "bold"}}>
-          Consider Donating
-          </Typography>
-        </Box>
+        <Typography sx={{ fontWeight: 'bold' }}>Building and maintaining a melodically enchanted realms takes a lot of mana! Consider donating the dev wizards some potions of awakening</Typography>
+        <Button variant="contained" color="primary" >Donate Here</Button>
       </Box>
-    );
-  };
-  export default AboutComponent;
+    </Box>
+  );
+};
+export default AboutComponent;
