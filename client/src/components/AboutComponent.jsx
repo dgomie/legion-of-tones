4e31e81528d42396f2c32b1a81e74a2ea7ee6f81
Danny <danny.f.gomez@gmail.com>
2024-08-06 14:11:20 -0400

    add stripe link to aboutcomponent

--- client/src/components/AboutComponent.jsx
+++ client/src/components/AboutComponent.jsx
@@ -1,15 +1,28 @@
 import { Box, Typography, Divider, Button } from '@mui/material';
 
 const AboutComponent = () => {
+  const handleDonateClick = () => {
+    window.location.href = 'https://donate.stripe.com/3csfZl65kbeE2FG6oo';
+  };
+
   return (
     <Box>
-      <Typography variant='h3' sx={{fontFamily: "medievalSharp"}}>   About Us</Typography>
+      <Typography variant="h3" sx={{ fontFamily: 'medievalSharp' }}>
+        About Us
+      </Typography>
       <Divider sx={{ marginY: 2 }} />
       <Box>
-        <Typography sx={{ fontWeight: 'bold' }}>Building and maintaining a melodically enchanted realms takes a lot of mana! Consider donating the dev wizards some potions of awakening</Typography>
-        <Button variant="contained" color="primary" >Donate Here</Button>
+        <Typography sx={{ fontWeight: 'bold' }}>
+          Building and maintaining a melodically enchanted realms takes a lot of
+          mana! Consider donating potions of awakening or even a coffee to help
+          the dev wizards slay bugs, maintain the realms, and expand the Legion.
+        </Typography>
+        <Button variant="contained" color="primary" onClick={handleDonateClick}>
+          Donate Here
+        </Button>
       </Box>
     </Box>
   );
 };
+
 export default AboutComponent;
