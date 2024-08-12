1362ef9c2eb35cf7ae1622b89388c1eb0ad35c7d
Danny <danny.f.gomez@gmail.com>
2024-08-12 10:24:18 -0400

    rearrange vote and playlist buttons

--- client/src/components/submissions/VoteSubmission.jsx
+++ client/src/components/submissions/VoteSubmission.jsx
@@ -2,32 +2,32 @@ import vote from '../../images/vote.svg';
 import playlist from '../../images/playlist.svg';
 import { Container, Button, Typography } from '@mui/material';
 
-const VoteComponent = ({legion, round}) => {
+const VoteComponent = ({ legion, round }) => {
   const handleVoteClick = () => {
     console.log('voted');
-    console.log("legion", legion)
-    console.log("round", round)
+    console.log('legion', legion);
+    console.log('round', round);
   };
 
   const handlePlaylistClick = () => {
     console.log('playlist');
-    console.log("legion", legion)
-    console.log("round", round)
+    console.log('legion', legion);
+    console.log('round', round);
   };
 
   return (
     <>
       <Container sx={{ display: 'flex', justifyContent: 'center' }}>
-        <Button onClick={handleVoteClick} sx={{ display: 'block' }}>
-          <img src={vote} width="45px" />
-          <Typography>Vote</Typography>
+        <Button onClick={handlePlaylistClick} sx={{ display: 'block' }}>
+          <img src={playlist} width="45px" />
+          <Typography>Listen to Playlist</Typography>
         </Button>
       </Container>
 
       <Container sx={{ display: 'flex', justifyContent: 'center' }}>
-        <Button onClick={handlePlaylistClick} sx={{ display: 'block' }}>
-          <img src={playlist} width="45px" />
-          <Typography>Listen to Playlist</Typography>
+        <Button onClick={handleVoteClick} sx={{ display: 'block' }}>
+          <img src={vote} width="45px" />
+          <Typography>Vote</Typography>
         </Button>
       </Container>
     </>
