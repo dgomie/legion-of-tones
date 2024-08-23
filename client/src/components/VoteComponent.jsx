ec5721eafc71cebb6a5e44bf28c7f898d2daf965
Danny <danny.f.gomez@gmail.com>
2024-08-23 11:39:24 -0400

    create vote component and import into vote page

---
+++ client/src/components/VoteComponent.jsx
@@ -0,0 +1,12 @@
+import { Container } from '@mui/material';
+
+const VoteComponent = () => {
+
+  return (
+    <Container sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+      vote
+    </Container>
+  );
+};
+
+export default VoteComponent;
