618b09acc608fb523a25e069a79ffad9adbeed15
Danny <danny.f.gomez@gmail.com>
2024-08-26 12:51:01 -0400

    create standings component

---
+++ client/src/components/StandingsComponent.jsx
@@ -0,0 +1,12 @@
+import { Container } from '@mui/material';
+
+const StandingsComponent = () => {
+
+  return (
+    <Container sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+      Standings
+    </Container>
+  );
+};
+
+export default StandingsComponent;
